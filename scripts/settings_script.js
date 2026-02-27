a313a500f4c6a819c2ffd2f063b72631e2c41903
ItzIsonn_ <kotov.e2473@gmail.com>
2026-02-27 22:15:17 +0300

    Added color hue slider (idk who needs this)

--- scripts/settings_script.js
+++ scripts/settings_script.js
@@ -1,19 +1,43 @@
 if (localStorage.getItem("theme") == null) localStorage.setItem("theme", window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (localStorage.getItem("hue") == null) localStorage.setItem("hue", "230");
 
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
     localStorage.setItem("theme", event.matches ? "dark" : "light");
-    updateColorScheme();
+    updateTheme();
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    updateColorScheme();
 
-    document.querySelector(".toggle_theme_button").addEventListener("click", () => {
-        localStorage.setItem("theme", localStorage.getItem("theme") === "dark" ? "light" : "dark");
-        updateColorScheme();
+document.addEventListener("DOMContentLoaded", () => {
+    themeTitleNode = document.querySelector(".settings_section .theme .title");
+
+    updateTheme();
+    updateHue();
+
+    document.querySelector(".settings_section .toggle_theme_button").addEventListener("click", () => {
+        localStorage.setItem("theme", localStorage.getItem("theme") === "light" ? "dark" : "light");
+        updateTheme();
+    });
+
+
+    const hueSliderNode = document.querySelector(".settings_section .hue_slider");
+    hueSliderNode.value = localStorage.getItem("hue");
+
+    hueSliderNode.addEventListener("input", () => {
+        localStorage.setItem("hue", hueSliderNode.value);
+        updateHue();
     });
 });
 
-function updateColorScheme() {
-    document.documentElement.style.colorScheme = localStorage.getItem("theme");
+
+
+let themeTitleNode
+
+function updateTheme() {
+    const theme = localStorage.getItem("theme");
+    themeTitleNode.innerText = theme === "light" ? "Светлая тема" : "Тёмная тема";
+    document.documentElement.style.colorScheme = theme;
+}
+
+function updateHue() {
+    document.documentElement.style.setProperty("--color-hue", localStorage.getItem("hue"));
 }
